496fba1e884b5d96a265d0e87dfe700375725722
Thomas Hehn <t.m.hehn@tudelft.nl>
2018-11-05 14:57:25 +0100

    Added bash script to run inference in corresponding conda environment.

---
+++ run_segmentation.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo -e "\n--- Running semantic segmentation."
+#cd ICNet-tensorflow
+cd $(dirname $0)
+pwd
+source activate tf
+echo "Executing:"\
+  "python inference.py --img-path=../${1} --save-dir=../${2}"\
+  "--model=train --dataset=cityscapes --filter-scale=1"
+python inference.py --img-path=../${1} --save-dir=../${2}\
+  --model=train --dataset=cityscapes --filter-scale=1
+source deactivate
+cd ..
+
